580e005903e4e7d462b4bf84cfd2a7a7d269415c
residentcyborg <sandras.simanauskas@icloud.com>
2020-03-19 21:10:52 +0200

    Initial commit

---
+++ Titanium.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+rm -f Titanium.flac
+nasm  -f macho64 -o Titanium.o Titanium.s
+ld -o Titanium -macosx_version_min 10.15 -lSystem Titanium.o
+./Titanium > Titanium.raw
+ffmpeg  -ac 2 -ar 44100 -f s16le -i Titanium.raw -compression_level 8 Titanium.flac
+metaflac --import-picture-from=Titanium.png --set-tag="Artist=Sandras" --set-tag="Title=Titanium" --set-tag="Date=2020" Titanium.flac
+rm -f Titanium Titanium.raw Titanium.o
