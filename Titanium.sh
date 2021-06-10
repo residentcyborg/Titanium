ae16dc622722052b442d18288fc23a70eb0a7dd9
residentcyborg <sandras.simanauskas@icloud.com>
2021-06-10 15:46:58 +0300

    Use make instead of a build script

--- Titanium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -f Titanium.flac
-nasm  -f macho64 -o Titanium.o Titanium.s
-ld -o Titanium -static Titanium.o
-./Titanium > Titanium.raw
-ffmpeg  -ac 2 -ar 44100 -f s16le -i Titanium.raw -compression_level 8 Titanium.flac
-metaflac --import-picture-from=Titanium.png --set-tag="Artist=Sandras" --set-tag="Title=Titanium" --set-tag="Date=2020" Titanium.flac
-rm -f Titanium Titanium.raw Titanium.o
